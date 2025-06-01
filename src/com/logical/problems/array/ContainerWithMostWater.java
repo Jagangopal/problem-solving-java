718d98b593e84d215cf5abad1011dbe450529bdd
Jagan Gopal Pattabiraman <pjagangopal@gmail.com>
2025-06-01 13:50:45 +0530

    two pointer problem ContainerWithMostWater

---
+++ src/com/logical/problems/array/ContainerWithMostWater.java
@@ -0,0 +1,28 @@
+package com.logical.problems.array;
+
+public class ContainerWithMostWater {
+
+    public int calculateContainerWithMostWater(int[] inputArray){
+        int left = 0, right = inputArray.length - 1;
+        int max = 0;
+        while(left < right){
+            int minValue = Math.min(inputArray[left], inputArray[right]);
+            int width = right - left;
+            int area = minValue * width;
+            if (max < area) {
+                max = area;
+            } else if(inputArray[left] < inputArray[right]) {
+                left++;
+            } else{
+                right--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args){
+        ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+        int[] inputArray = {1,8,6,2,5,4,8,3,7};
+        System.out.println(containerWithMostWater.calculateContainerWithMostWater(inputArray));
+    }
+}
